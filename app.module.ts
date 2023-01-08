3efbb75149a1ab9d9c1422e90b85487ee8aee862
mohammedkarim69! <mohammed.karim.bouzidi@gmail.com>
2023-01-08 14:35:08 +0100

    first commit

---
+++ app.module.ts
@@ -0,0 +1,21 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BlockComponentComponent } from './block-component/block-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlockComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
